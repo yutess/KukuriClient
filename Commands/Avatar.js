f7638eacf7e40a0c659dcfc13f50013d4710cc70
Kukuri and the Harmonic Odyssey <83948007+Mikasuru@users.noreply.github.com>
2025-01-04 19:14:56 +0700

    Delete Commands directory

--- Commands/Avatar.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'avatar',
-    description: 'Get user\'s avatar',
-    execute(message, args, client) {
-        const user = message.mentions.users.first() || message.author;
-        message.reply(user.displayAvatarURL({ dynamic: true, size: 4096 }));
-    },
-};
